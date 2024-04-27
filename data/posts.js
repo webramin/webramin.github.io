1c9ec38ffc97f57d9abe818ad9868bf6eb052545
WEB RAMIN <88180002+webramin@users.noreply.github.com>
2024-04-27 08:24:07 +0330

    Create posts.js

---
+++ data/posts.js
@@ -0,0 +1,19 @@
+const posts = [
+  {
+    "id": 1,
+    "title": "کشف آینده هوش مصنوعی با آزمایشگاه های Perplexity",
+    "description": "آخرین پیشرفت‌های هوش مصنوعی و یادگیری ماشینی را در آزمایشگاه‌های Perplexity کشف کنید. از پردازش زبان طبیعی تا بینایی کامپیوتری، تیم ما مرزهای امکان‌پذیر را گسترش می‌دهد",
+    "image": "https://example.com/image1.jpg",
+    "link": "https://labs.perplexity.ai/",
+    "tags": "ai, text-ai, GPT-4, site"
+  },
+  {
+    "id": 2,
+    "title": "عنوان پست دوم",
+    "description": "توضیحات مربوط به پست دوم. این پست در مورد موضوع دیگری است.",
+    "image": "https://example.com/image2.jpg",
+    "link": "https://example.com/post2",
+    "tags": "jQuery, bootstrap"
+  }
+  // و بقیه داده های JSON
+];
