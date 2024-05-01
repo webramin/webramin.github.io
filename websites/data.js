449ac597578ad0fb6e18e6d106a6b79120d43a76
WEB RAMIN <88180002+webramin@users.noreply.github.com>
2024-05-01 20:40:55 +0330

    Update and rename data.json to data.js

---
+++ websites/data.js
@@ -0,0 +1,19 @@
+const datapost = [
+  // {
+  //   id: 2,
+  //   title: " نام پست بعد",
+  //   description: " توضیحات ",
+  //   image: "https://example.com/image1.jpg",
+  //   link: "https://labs.perplexity.ai/",
+  //   tags: "تگ ها , tag "
+  // },
+  
+  {
+    id: 1,
+    title: "کشف آینده هوش مصنوعی با آزمایشگاه های Perplexity",
+    description: "آخرین پیشرفت‌های هوش مصنوعی و یادگیری ماشینی را در آزمایشگاه‌های Perplexity کشف کنید. از پردازش زبان طبیعی تا بینایی کامپیوتری، تیم ما مرزهای امکان‌پذیر را گسترش می‌دهد",
+    image: "https://example.com/image1.jpg",
+    link: "https://labs.perplexity.ai/",
+    tags: "ai, text-ai, GPT-4, site"
+  }
+]
