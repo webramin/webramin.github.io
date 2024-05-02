6a380b0d76f8d28289b905324a3815bc8bc6cc67
WEB RAMIN <88180002+webramin@users.noreply.github.com>
2024-05-02 17:49:08 +0330

    Create data.js

---
+++ other/data.js
@@ -0,0 +1,35 @@
+
+datapost = [
+  {
+    id: 4,
+    title: "4 متن کوتاه",
+    description: "متن کوتاه و بی معنی",
+    image: "https://example.com/image1.jpg",
+    link: "https://www.example.com/article1",
+    tags: "متن, بی معنی, کوتاه"
+  },
+   {
+    id: 3,
+    title: "3 متن کوتاه",
+    description: "متن کوتاه و بی معنی",
+    image: "https://example.com/image1.jpg",
+    link: "https://www.example.com/article2",
+    tags: "متن, بی معنی, کوتاه"
+  },
+   {
+    id: 2,
+    title: "2 متن کوتاه",
+    description: "متن کوتاه و بی معنی",
+    image: "https://example.com/image1.jpg",
+    link: "https://www.example.com/article3",
+    tags: "متن, بی معنی, کوتاه"
+  },
+   {
+    id: 1,
+    title: "1 متن کوتاه",
+    description: "متن کوتاه و بی معنی",
+    image: "https://example.com/image1.jpg",
+    link: "https://www.example.com/article4",
+    tags: "متن, بی معنی, کوتاه"
+  }
+]
