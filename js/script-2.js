e9b6608e932c7f7bcd6389f06919691cca90ad0d
WEB RAMIN <88180002+webramin@users.noreply.github.com>
2024-05-02 10:36:11 +0330

    Update script-2.js

--- js/script-2.js
+++ js/script-2.js
@@ -98,13 +98,62 @@ $(document).ready(function() {
 
   // تاریخ شمسی را نمایش می‌دهد
   function showdate() {
-    // ... (کد موجود)
+  week = new Array("یکشنبه", "دوشنبه", "سه‌شنبه", "چهارشنبه", "پنج‌شنبه", "جمعه", "شنبه");
+  months = new Array("فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند");
+  a = new Date();
+  d = a.getDay();
+  day = a.getDate();
+  month = a.getMonth()+1;
+  year = a.getYear()-100;
+  year = (year == 0)?2000: year;
+  (year < 1000)? (year += 2000): true;
+  year -= ((month < 3) || ((month == 3) && (day < 21)))? 622: 621;
+  switch (month) {
+    case 1: (day < 21)? (month = 10, day += 10): (month = 11, day -= 20); break;
+    case 2: (day < 20)? (month = 11, day += 11): (month = 12, day -= 19); break;
+    case 3: (day < 21)? (month = 12, day += 9): (month = 1, day -= 20); break;
+    case 4: (day < 21)? (month = 1, day += 11): (month = 2, day -= 20); break;
+    case 5:
+    case 6: (day < 22)? (month -= 3, day += 10): (month -= 2, day -= 21); break;
+    case 7:
+    case 8:
+    case 9: (day < 23)? (month -= 3, day += 9): (month -= 2, day -= 22); break;
+    case 10: (day < 23)? (month = 7, day += 8): (month = 8, day -= 22); break;
+    case 11:
+    case 12: (day < 22)? (month -= 3, day += 9): (month -= 2, day -= 21); break;
+    default: break;
   }
+  return week[d]+" "+day+" "+months[month-1]+" "+ year;
+}
   $('#persianDate').text(showdate());
 
   // تاریخ میلادی را نمایش می‌دهد
-  function showGregorianDate() {
-    // ... (کد موجود)
+ function showGregorianDate() {
+  const weekDays = ["یکشنبه",
+    "دوشنبه",
+    "سه‌شنبه",
+    "چهارشنبه",
+    "پنج‌شنبه",
+    "جمعه",
+    "شنبه"];
+  const months = ["ژانویه",
+    "فوریه",
+    "مارس",
+    "آوریل",
+    "مه",
+    "ژوئن",
+    "ژوئیه",
+    "آگوست",
+    "سپتامبر",
+    "اکتبر",
+    "نوامبر",
+    "دسامبر"];
+  const today = new Date();
+  const dayOfWeek = weekDays[today.getDay()];
+  const day = today.getDate();
+  const month = months[today.getMonth()];
+  const year = today.getFullYear();
+  return `${dayOfWeek} ${day} ${month} ${year}`;
   }
   $('#gregorianDate').text(showGregorianDate());
 
