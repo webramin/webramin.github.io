01b84f99d7a35279af1ba509e85269e17278fd73
WEB RAMIN <88180002+webramin@users.noreply.github.com>
2024-05-03 15:46:16 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -197,6 +197,22 @@ function displayPostsFromJSON() {
   `);
 }
 
+
+    var navPostUl = $("#nav-post-ul");
+    var lastFivePosts = datapost.slice(-5);
+    $.each(lastFivePosts, function(index, post) {
+      var listItem = $("<li>")
+        .addClass("nav-item mb-2")
+        .append($("<a>")
+          .addClass("nav-link p-0 text-body-secondary")
+          .attr("href", post.link)
+          .text(post.title)
+        );
+      navPostUl.append(listItem);
+    });
+
+
+
     function goBack() {
       window.history.back();
     }
