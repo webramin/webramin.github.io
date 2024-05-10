4f91084656e2f4717c9e730c2fdc2eb29a01d785
WEB RAMIN <88180002+webramin@users.noreply.github.com>
2024-05-10 15:49:53 +0330

    Update post-list.js
    Signed-off-by: WEB RAMIN <88180002+webramin@users.noreply.github.com>

--- js/post-list.js
+++ js/post-list.js
@@ -25,6 +25,26 @@ var datapost = [
   }
 ];
 
+$(document).ready(function() {
+  var navPostUl = $("#nav-post-ul");
+
+  $.getJSON("data.json", function(data) {
+    var lastFivePosts = data.slice(0, 5).reverse();
+    $.each(lastFivePosts, function(index, post) {
+      var listItem = $("<li>")
+        .addClass("nav-item mb-2")
+        .append($("<a target='_blank'>")
+          .addClass("nav-link p-0 text-body-secondary")
+          .attr("href", post.link)
+          .text(post.title)
+        );
+      navPostUl.append(listItem);
+    });
+  });
+});
+
+
+
 var navPostUl = $("#nav-post-ul");
 var lastFivePosts = datapost.slice(0, 5).reverse();
 $.each(lastFivePosts, function(index, post) {
