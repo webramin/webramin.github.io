f075aa0c8646055bca06198f5f61c078567b2e9a
WEB RAMIN <88180002+webramin@users.noreply.github.com>
2024-05-10 15:58:51 +0330

    Update post-list.js
    Signed-off-by: WEB RAMIN <88180002+webramin@users.noreply.github.com>

--- js/post-list.js
+++ js/post-list.js
@@ -1,59 +1,15 @@
-// var datapost = [
-//   {
-//     title: "پست 6",
-//     link: "#"
-//   },
-//   {
-//     title: "پست 5",
-//     link: "#"
-//   },
-//   {
-//     title: "پست 4",
-//     link: "#"
-//   },
-//   {
-//     title: "پست 3",
-//     link: "#"
-//   },
-//   {
-//     title: "پست 2",
-//     link: "#"
-//   },
-//   {
-//     title: "پست 1",
-//     link: "#"
-//   }
-// ];
-
 $(document).ready(function() {
   var navPostUl = $("#nav-post-ul");
 
-  $.getJSON("data.json", function(data) {
-    var lastFivePosts = data.slice(0, 5).reverse();
-    $.each(lastFivePosts, function(index, post) {
-      var listItem = $("<li>")
-        .addClass("nav-item mb-2")
-        .append($("<a target='_blank'>")
-          .addClass("nav-link p-0 text-body-secondary")
-          .attr("href", post.link)
-          .text(post.title)
-        );
-      navPostUl.append(listItem);
-    });
+  const lastFivePosts = datapost.slice(0, 5);
+  $.each(lastFivePosts, function(index, post) {
+    var listItem = $("<li>")
+      .addClass("nav-item mb-2")
+      .append($("<a target='_blank'>")
+        .addClass("nav-link p-0 text-body-secondary")
+        .attr("href", post.link)
+        .text(post.title)
+      );
+    navPostUl.prepend(listItem);
   });
 });
-
-
-
-// var navPostUl = $("#nav-post-ul");
-// var lastFivePosts = datapost.slice(0, 5).reverse();
-// $.each(lastFivePosts, function(index, post) {
-//   var listItem = $("<li>")
-//     .addClass("nav-item mb-2")
-//     .append($("<a target='_blank'>")
-//       .addClass("nav-link p-0 text-body-secondary")
-//       .attr("href", post.link)
-//       .text(post.title)
-//     );
-//   navPostUl.append(listItem);
-// });
