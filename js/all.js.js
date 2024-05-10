2273d7260bb399ac136ef8179d69edc17e801ba2
WEB RAMIN <88180002+webramin@users.noreply.github.com>
2024-05-10 15:47:46 +0330

    Create all.js.js
    Signed-off-by: WEB RAMIN <88180002+webramin@users.noreply.github.com>

---
+++ js/all.js.js
@@ -0,0 +1,31 @@
+// <script src="https://webramin.github.io/js/all-js.js"></script>
+
+// Load AOS
+var aos = document.createElement('script');
+aos.src = 'https://unpkg.com/aos@2.3.1/dist/aos.js';
+document.body.appendChild(aos);
+
+// Load Bootstrap Bundle
+var bootstrapBundle = document.createElement('script');
+bootstrapBundle.src = 'https://webramin.github.io/js/bootstrap.bundle.js';
+document.body.appendChild(bootstrapBundle);
+
+// Load Color Modes
+var colorModes = document.createElement('script');
+colorModes.src = 'https://webramin.github.io/js/color-modes.js';
+document.body.appendChild(colorModes);
+
+// Load Popper
+var popper = document.createElement('script');
+popper.src = 'https://webramin.github.io/js/popper.min.js';
+document.body.appendChild(popper);
+
+// Load Script
+var script = document.createElement('script');
+script.src = 'https://webramin.github.io/js/script.js';
+document.body.appendChild(script);
+
+// Load Post List
+var postList = document.createElement('script');
+postList.src = 'https://webramin.github.io/js/post-list.js';
+document.body.appendChild(postList);
