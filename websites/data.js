261d58d0ac67b200e8e3cbfe5b58b759290589dd
WEB RAMIN <88180002+webramin@users.noreply.github.com>
2024-05-22 11:30:56 +0330

    Update data.js
    Signed-off-by: WEB RAMIN <88180002+webramin@users.noreply.github.com>

--- websites/data.js
+++ websites/data.js
@@ -1,42 +1,30 @@
 const datapost = [
   {
-    "id": 1,
-    "title": "کشف آینده هوش مصنوعی با آزمایشگاه های Perplexity",
-    "description": "آخرین پیشرفت‌های هوش مصنوعی و یادگیری ماشینی را در آزمایشگاه‌های Perplexity کشف کنید. از پردازش زبان طبیعی تا بینایی کامپیوتری، تیم ما مرزهای امکان‌پذیر را گسترش می‌دهد",
-    "image": "https://example.com/image1.jpg",
-    "link": "https://labs.perplexity.ai/",
-    "tags": "ai, text-ai, GPT-4, site",
-    "date":"1403/01/24",
-    "بزار": "بزار"
+    id: 1,
+    title: "کشف آینده هوش مصنوعی با آزمایشگاه های Perplexity",
+    description: "آخرین پیشرفت‌های هوش مصنوعی و یادگیری ماشینی را در آزمایشگاه‌های Perplexity کشف کنید. از پردازش زبان طبیعی تا بینایی کامپیوتری، تیم ما مرزهای امکان‌پذیر را گسترش می‌دهد",
+    image: "https://example.com/image1.jpg",
+    link: "https://labs.perplexity.ai/",
+    tags: "ai, text-ai, GPT-4, site"
   },
   {
     "id": 2,
     "title": "Vondy",
     "description": "ونی یک پلتفرم است که مجموعه ای از ابزارهای هوش مصنوعی را برای کمک به انجام وظایف مختلف مانند نوشتن مقالات جذاب، تولید برنامه های تناسب اندام، ترجمه زبان ها و موارد دیگر ارائه می دهد. این پلتفرم یک مرکز یکپارچه برای ابزارهای هوش مصنوعی فراهم می کند که مدیریت و استفاده از قابلیت های هوش مصنوعی را آسان تر می سازد.",
-    "image": "#",
+    "image": "",
     "link": "https://www.vondy.com/",
-    "tags": "AI, Productivity, Writing, Translation, Fitness",
-    "date": "1403/02/10",
-    "بزار": "بزار"
+    "tags": "AI, Productivity, Writing, Translation, Fitness"
   },
-  {
+  
+{
     "id": 3,
     "title": "chat imsys",
     "description": "یک هوش مصنوعی را برای کمک به انجام وظایف مختلف مانند نوشتن مقالات جذاب، تولید برنامه های تناسب اندام.",
-    "image": "#",
+    "image": "",
     "link": "https://chat.lmsys.org/",
     "tags": "AI, Cloud 3, text-ai",
-    "date": "1403/02/30",
-    "بزار": "بزار"
-  },
-  {
-    "id": 4,
-    "title": "claude.ai",
-    "description": "Claude.ai یک شرکت هوش مصنوعی است که بر توسعه مدل‌های زبانی پیشرفته و سیستم‌های گفتگوی هوشمند تمرکز دارد و متعهد به ایجاد فناوری‌های هوش مصنوعی اخلاقی و همسو با ارزش‌های انسانی است.",
-    "image": "#",
-    "link": "https://claude.ai/",
-    "tags": "AI, Cloud 3, text-ai",
-    "date": "1403/03/02",
-    "بزار": "بزار"
+    "date": "1403/02/30"
   }
+ 
+  
 ]
