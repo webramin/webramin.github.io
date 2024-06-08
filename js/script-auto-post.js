f3812cd5a4d325d38ae2813469eb8cf7798ff809
WEB RAMIN <88180002+webramin@users.noreply.github.com>
2024-06-08 10:29:36 +0330

    Update script-auto-post.js
    Signed-off-by: WEB RAMIN <88180002+webramin@users.noreply.github.com>

--- js/script-auto-post.js
+++ js/script-auto-post.js
@@ -89,3 +89,22 @@ $('.tag-btn').click(function() {
 });
 
 loadPosts(currentPage);
+
+ const navPostUl = $("#nav-post-ul");
+
+      // دریافت پنج پست آخر
+      const lastFivePosts = datapost.slice(0, 5);
+
+      // ایجاد آیتم‌های لیست
+      lastFivePosts.forEach(post => {
+        const listItem = $("<li>").addClass("nav-item mb-2");
+        const link = $("<a>")
+          .addClass("nav-link p-0 text-body-secondary")
+          .attr("href", post.link)
+          .attr("target", "_blank")
+          .text(post.title);
+
+        listItem.append(link);
+        navPostUl.prepend(listItem);
+      });
+    });
