4b5f3bd94ea84a9da67680943d87fc811d53aaa0
WEB RAMIN <88180002+webramin@users.noreply.github.com>
2024-12-26 00:29:17 +0330

    Update app.js
    Signed-off-by: WEB RAMIN <88180002+webramin@users.noreply.github.com>

--- js/app.js
+++ js/app.js
@@ -100,32 +100,31 @@ $('title').text(newTitle);
 
 // تابع تاریخ شمسی
 function showdate() {
-  week = new Array("یکشنبه", "دوشنبه", "سه‌شنبه", "چهارشنبه", "پنج‌شنبه", "جمعه", "شنبه");
-  months = new Array("فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند");
-  a = new Date();
-  d = a.getDay();
-  day = a.getDate();
-  month = a.getMonth()+1;
-  year = a.getYear()-100;
-  year = (year == 0)?2000: year;
-  (year < 1000)? (year += 2000): true;
-  year -= ((month < 3) || ((month == 3) && (day < 21)))? 622: 621;
+  const week = ["یکشنبه", "دوشنبه", "سه‌شنبه", "چهارشنبه", "پنج‌شنبه", "جمعه", "شنبه"];
+  const months = ["فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند"];
+  const a = new Date();
+  let d = a.getDay();
+  let day = a.getDate();
+  let month = a.getMonth() + 1;
+  let year = a.getFullYear() - 621;
+  if ((month < 3) || ((month == 3) && (day < 21))) year -= 1;
+  
   switch (month) {
-    case 1: (day < 21)? (month = 10, day += 10): (month = 11, day -= 20); break;
-    case 2: (day < 20)? (month = 11, day += 11): (month = 12, day -= 19); break;
-    case 3: (day < 21)? (month = 12, day += 9): (month = 1, day -= 20); break;
-    case 4: (day < 21)? (month = 1, day += 11): (month = 2, day -= 20); break;
+    case 1: day < 21 ? (month = 10, day += 10) : (month = 11, day -= 20); break;
+    case 2: day < 20 ? (month = 11, day += 11) : (month = 12, day -= 19); break;
+    case 3: day < 21 ? (month = 12, day += 9) : (month = 1, day -= 20); break;
+    case 4: day < 21 ? (month = 1, day += 11) : (month = 2, day -= 20); break;
     case 5:
-    case 6: (day < 22)? (month -= 3, day += 10): (month -= 2, day -= 21); break;
+    case 6: day < 22 ? (month -= 3, day += 10) : (month -= 2, day -= 21); break;
     case 7:
     case 8:
-    case 9: (day < 23)? (month -= 3, day += 9): (month -= 2, day -= 22); break;
-    case 10: (day < 23)? (month = 7, day += 8): (month = 8, day -= 22); break;
+    case 9: day < 23 ? (month -= 3, day += 9) : (month -= 2, day -= 22); break;
+    case 10: day < 23 ? (month = 7, day += 8) : (month = 8, day -= 22); break;
     case 11:
-    case 12: (day < 22)? (month -= 3, day += 9): (month -= 2, day -= 21); break;
+    case 12: day < 22 ? (month -= 3, day += 9) : (month -= 2, day -= 21); break;
     default: break;
   }
-  return week[d]+" "+day+" "+months[month-1]+" "+ year;
+  return week[d] + " " + day + " " + months[month - 1] + " " + year;
 }
 document.getElementById('persianDate').innerText = showdate();
 
