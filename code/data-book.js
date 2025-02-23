df93f3355199d96221c7fad63652d952f13048a2
WEB RAMIN <88180002+webramin@users.noreply.github.com>
2025-02-24 00:34:15 +0330

    Create data-book.js
    Signed-off-by: WEB RAMIN <88180002+webramin@users.noreply.github.com>

---
+++ code/data-book.js
@@ -0,0 +1,94 @@
+<script>
+            jQuery.extend(Drupal.settings, {
+                "basePath": "\/",
+                "pathPrefix": "",
+                "setHasJsCookie": 0,
+                "ajaxPageState": {
+                    "theme": "chap_resp",
+                    "theme_token": "ITJPov6iooekLA1AnnfJfSQcmNCuxVDbxO3ttT9fdxc",
+                    "jquery_version": "3.6.0",
+                    "js": {
+                        "https:\/\/code.jquery.com\/jquery-3.6.0.js": 1,
+                        "misc\/jquery-extend-3.4.0.js": 1,
+                        "misc\/jquery-html-prefilter-3.5.0-backport.js": 1,
+                        "misc\/jquery.once.js": 1,
+                        "misc\/drupal.js": 1,
+                        "sites\/all\/modules\/jquery_update\/js\/jquery_browser.js": 1,
+                        "https:\/\/cdn.jsdelivr.net\/gh\/carhartl\/jquery-cookie@1.4.1\/jquery.cookie.js": 1,
+                        "https:\/\/cdn.jsdelivr.net\/gh\/jquery-form\/form@4.2.1\/dist\/jquery.form.min.js": 1,
+                        "misc\/form-single-submit.js": 1,
+                        "misc\/ajax.js": 1,
+                        "sites\/all\/modules\/jquery_update\/js\/jquery_update.js": 1,
+                        "public:\/\/languages\/fa_uO5zW0aTM-knxFyuDB8CYvxczlrl4zPNGP1luqOAALM.js": 1,
+                        "sites\/all\/modules\/views\/js\/base.js": 1,
+                        "misc\/progress.js": 1,
+                        "sites\/all\/modules\/responsive_menus\/styles\/responsive_menus_simple\/js\/responsive_menus_simple.js": 1,
+                        "sites\/all\/modules\/views\/js\/ajax_view.js": 1,
+                        "sites\/all\/themes\/chap_resp\/filter-hierarchy-select.js": 1,
+                        "sites\/all\/themes\/chap_resp\/js\/treelist.js": 1
+                    },
+                    "css": {
+                        "modules\/system\/system.base.css": 1,
+                        "modules\/system\/system.base-rtl.css": 1,
+                        "modules\/system\/system.menus.css": 1,
+                        "modules\/system\/system.menus-rtl.css": 1,
+                        "modules\/system\/system.messages.css": 1,
+                        "modules\/system\/system.messages-rtl.css": 1,
+                        "modules\/system\/system.theme.css": 1,
+                        "modules\/system\/system.theme-rtl.css": 1,
+                        "modules\/field\/theme\/field.css": 1,
+                        "modules\/field\/theme\/field-rtl.css": 1,
+                        "modules\/node\/node.css": 1,
+                        "modules\/poll\/poll.css": 1,
+                        "modules\/poll\/poll-rtl.css": 1,
+                        "modules\/user\/user.css": 1,
+                        "modules\/user\/user-rtl.css": 1,
+                        "sites\/all\/modules\/views\/css\/views.css": 1,
+                        "sites\/all\/modules\/views\/css\/views-rtl.css": 1,
+                        "sites\/all\/modules\/ctools\/css\/ctools.css": 1,
+                        "sites\/all\/modules\/responsive_menus\/styles\/responsive_menus_simple\/css\/responsive_menus_simple.css": 1,
+                        "sites\/all\/themes\/chap_resp\/system.menus.css": 1,
+                        "sites\/all\/themes\/chap_resp\/system.messages.css": 1,
+                        "sites\/all\/themes\/chap_resp\/system.theme.css": 1,
+                        "sites\/all\/themes\/chap_resp\/css\/styles.css": 1,
+                        "sites\/all\/themes\/chap_resp\/css\/styles-rtl.css": 1
+                    }
+                },
+                "urlIsAjaxTrusted": {
+                    "\/advanced_search": true,
+                    "\/views\/ajax": true
+                },
+                "views": {
+                    "ajax_path": "\/views\/ajax",
+                    "ajaxViews": {
+                        "views_dom_id:50e0c1f22f98bf78586deb18855805c6": {
+                            "view_name": "book_search",
+                            "view_display_id": "page_1",
+                            "view_args": "",
+                            "view_path": "advanced_search",
+                            "view_base_path": "advanced_search",
+                            "view_dom_id": "50e0c1f22f98bf78586deb18855805c6",
+                            "pager_element": "13"
+                        },
+                        "views_dom_id:ada13418c88d46913c7293fddf8b8b28": {
+                            "view_name": "recent_book_view",
+                            "view_display_id": "block_1",
+                            "view_args": "",
+                            "view_path": "advanced_search",
+                            "view_base_path": "recent_book_view",
+                            "view_dom_id": "ada13418c88d46913c7293fddf8b8b28",
+                            "pager_element": 0
+                        }
+                    }
+                },
+                "responsive_menus": [{
+                    "toggler_text": "\u2630 \u0641\u0647\u0631\u0633\u062a",
+                    "selectors": ["#main-menu"],
+                    "media_size": "768",
+                    "media_unit": "px",
+                    "absolute": "1",
+                    "remove_attributes": "1",
+                    "responsive_menus_style": "responsive_menus_simple"
+                }]
+            });
+        </script>
